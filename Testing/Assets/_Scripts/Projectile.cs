33c15ec225ad29075d3079e164532204b9d3b888
Luke's Laptop <meistermayo@gmail.com>
2018-03-10 11:46:50 -0600

    Wewapons

---
+++ Testing/Assets/_Scripts/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour {
+    Rigidbody body;
+    [SerializeField] Bullet bullet;
+    void Awake()
+    {
+        Destroy(gameObject,10f);
+        body = GetComponent<Rigidbody>();
+    }
+
+    public void SetBullet(Bullet bullet)
+    {
+        body = GetComponent<Rigidbody>();
+        this.bullet = bullet;
+        Debug.Log(bullet.moveSpeed);
+        body.velocity = transform.forward * bullet.moveSpeed;
+        body.velocity = Quaternion.Euler(new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f))*bullet.inaccuracy) * body.velocity;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Testing/Assets/_Scripts/WeaponManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    [SerializeField] Weapon weapon;
+    [SerializeField] Bullet bullet;
+    [SerializeField] GameObject baseProjectile;
+    Collider myCol;
+    float timer;
+    bool canShoot = true;
+    void Start()
+    {
+        myCol = GetComponent<Collider>();
+    }
+    void Update()
+    {
+        Shoot();
+        Timer();
+    }
+
+    void Shoot()
+    {
+        if (canShoot)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                GameObject temp = Instantiate(baseProjectile, Camera.main.transform.position, Camera.main.transform.rotation);
+                Physics.IgnoreCollision(myCol, temp.GetComponent<Collider>());
+                Projectile projectile = temp.GetComponent<Projectile>();
+                projectile.SetBullet(bullet);
+                canShoot = false;
+                timer = weapon.cooldown;
+            }
+        }
+    }
+
+    void Timer()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+            canShoot = true;
+    }
+}
